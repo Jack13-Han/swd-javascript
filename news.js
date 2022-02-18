275cf43c75385cdb9a9407e8ce07298750eb253b
hanwaihtun <hanwaihtun1e@gmail.com>
2022-02-18 19:51:35 +0630

    JavaScript SWD

---
+++ news.js
@@ -0,0 +1,266 @@
+let data = {
+    "status": "ok",
+    "totalResults": 13338,
+    "articles": [
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "The Motley Fool",
+    "title": "Bitcoin Coming Under Fire by China: Value Craters",
+    "description": "Bitcoin could fall into a giant crater as the U.S. and China cracks the whip on the cryptocurrencies. Canadian investors looking for superior gains should consider Mogo stock instead. The post Bitcoin Coming Under Fire by China: Value Craters appeared first o…",
+    "url": "https://ca.finance.yahoo.com/news/bitcoin-coming-under-fire-china-143049973.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/mjPq.Yg2AYsVwG9GqeQgcA--~B/aD01NjI7dz0xMDAwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/the_motley_fool_261/436b3803b1abc442b81ca9429388be51",
+    "publishedAt": "2021-07-03T14:30:49Z",
+    "content": "Bitcoin was on fire since January 2021 but is now under fire at the beginning of the third quarter. Crypto investors went crazy when the worlds most popular digital currency zoomed to as high as US$6… [+4167 chars]"
+    },
+    {
+    "source": {
+    "id": "la-repubblica",
+    "name": "La Repubblica"
+    },
+    "author": "repubblicawww@repubblica.it (Redazione Repubblica.it)",
+    "title": "La battaglia della CO2: così la Corolla batte la Tesla",
+    "description": "Il confronto tra la Model 3 e l'auto più venduta al mondo. Considerando l'intero ciclo di vita, comprese produzione e smaltimento delle batterie, si scopre un clamoroso risultato",
+    "url": "https://www.repubblica.it/motori/sezioni/tecnologia-e-ambiente/2021/07/03/news/la_battaglia_della_co2_cosi_la_corolla_batte_la_tesla-308741298/",
+    "urlToImage": "https://www.repstatic.it/content/nazionale/img/2021/07/03/161453624-57bd3d83-01f0-4f05-a3b5-a766f37e5407.jpg",
+    "publishedAt": "2021-07-03T14:17:04Z",
+    "content": "ROMA - La guerra delle emissioni continua. Emette più CO2 un'ibrida oppure un'elettrica? La risposta sembra scontata a favore dell'auto a batteria e invece... Il problema è tutt'altro che semplice. S… [+3085 chars]"
+    },
+    {
+    "source": {
+    "id": "breitbart-news",
+    "name": "Breitbart News"
+    },
+    "author": "Kurt Zindulka",
+    "title": "Revealed: Elon Musk, Tesla Go Full Shill for Communists on Chinese Social Media Site Weibo",
+    "description": "Elon Musk and his electric car company Tesla have shared full-blown praise of the Chinese Communist Party on Weibo, Breitbart News can reveal.",
+    "url": "https://www.breitbart.com/europe/2021/07/03/revealed-elon-musk-tesla-go-full-shill-for-communist-china-on-chinese-social-media-site-weibo/",
+    "urlToImage": "https://media.breitbart.com/media/2021/07/Untitled-design-5-2-640x335.jpeg",
+    "publishedAt": "2021-07-03T14:12:20Z",
+    "content": "Billionaire entrepreneur Elon Musk and his electric car company Tesla have shared full-blown praise of the Chinese Communist Party on China’s equivalent to Twitter, Breitbart News can reveal.\r\nThe ve… [+3834 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "HipHopDX"
+    },
+    "author": "Trent Clark",
+    "title": "Master P's Son Hercy Miller Signs $2M Deal 24 Hours After NCAA Allows Athletes To Earn Money",
+    "description": "The incoming Tennessee State basketball star freshman is also starting a camp to show the younger generation there's no limit to their dreams.",
+    "url": "https://hiphopdx.com/news/id.62993/title.master-p-son-hercy-miller-signs-2m-deal-24-hours-after-ncaa-allows-athletes-to-earn-money",
+    "urlToImage": "https://static.hiphopdx.com/2021/07/Screen-Shot-2021-07-03-at-6.48.43-AM-e1625320221681-1200x675.png",
+    "publishedAt": "2021-07-03T14:07:09Z",
+    "content": "Los Angeles, CA – The National Collegiate Athletic Association (NCAA) controversially changed its rules on Wednesday (June 30) to allow its Division 1 athletes to earn income after nearly 50 years of… [+2213 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Rolling Out"
+    },
+    "author": "Michael \"Ice-Blue\" Harris",
+    "title": "Master P uses new NCAA rules to help son ink $2M deal",
+    "description": "Master P is making generational wealth a priority in his family and has secured a $2 million endorsement deal for his 19-year-old son Hercy Miller. A deal had already been…\nThe post Master P uses new NCAA rules to help son ink $2M deal appeared first on Rolli…",
+    "url": "https://rollingout.com/2021/07/03/master-p-uses-new-ncaa-rules-to-help-son-ink-2m-deal/",
+    "urlToImage": "https://rollingout.com/wp-content/uploads/2021/03/master-p-sons.png",
+    "publishedAt": "2021-07-03T13:58:08Z",
+    "content": "Master P is making generational wealth a priority in his family and has secured a $2 million endorsement deal for his 19-year-old son Hercy Miller. A deal had already been in the works, but P was wai… [+1827 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Dagospia.com"
+    },
+    "author": null,
+    "title": "I MULTIMILIARDARI HANNO IL RAZZO DRITTO PER LO SPAZIO - RICHARD BRANSON ANNUNCIA CHE L’11 LUGLIO SALIRÀ SUL PRIMO VOLO DIRETTO NELLO SPAZIO, STRAPPANDO IL RECORD A BEZOS E ELON MUSK COME IL PRIMO MILIARDARIO IN ORBITA – CON LA “SPACEX” CHE HA GIA HA OTTENUTO LA CERTIFICAZIONE DELLA NASA PER IL TRASPORTO DI ASTRONAUTI DA TERRA ALLA STAZIONE SPAZIALE (E CERCA DI COLONIZZARE MARTE), L'OBIETTIVO DI ELON MUSK E'...",
+    "description": "MUSK BEZOS BRANSON SPAZIO Anna Guaita per “il Messaggero”   Una volta erano le superpotenze a gareggiare nello spazio. Ora sono i miliardari. Dall' alto dei loro patrimoni vasti come il Pil di interi Paesi, Elon Musk, Jeff Bezos e Richard Branson talvolta sem…",
+    "url": "https://www.dagospia.com/rubrica-29/cronache/multimiliardari-hanno-razzo-dritto-spazio-nbsp-nbsp-richard-275458.htm",
+    "urlToImage": "https://cdn-static.dagospia.com/img/patch/07-2021/bezos-musk-branson-spazio-razzo-1488215.png",
+    "publishedAt": "2021-07-03T13:36:00Z",
+    "content": "MUSK BEZOS BRANSON SPAZIO\r\nAnna Guaita per “il Messaggero”\r\nUna volta erano le superpotenze a gareggiare nello spazio. Ora sono i miliardari.\r\nDall' alto dei loro patrimoni vasti come il Pil di inter… [+3384 chars]"
+    },
+    {
+    "source": {
+    "id": "the-times-of-india",
+    "name": "The Times of India"
+    },
+    "author": "Basit Aijaz",
+    "title": "2nd Richest Person On Earth With Networth of $167B, Elon Musk Says He Lives In A $50,000 Rented Apartment",
+    "description": "Normally, you did expect a billionaire to have a lavish home, but Elon Musk isn't like your usual billionaire. The eccentric figure often does things differently and it often shows. Elon Musk may be the second richest person in the world with a personal fortu…",
+    "url": "https://www.indiatimes.com/trending/human-interest/elon-musk-lives-in-a-50000-rented-house-544156.html",
+    "urlToImage": "https://im.indiatimes.in/content/2021/Jul/EMH_60e062e289730.jpg",
+    "publishedAt": "2021-07-03T13:23:43Z",
+    "content": "Normally, you did expect a billionaire to have a lavish home, but Elon Musk isn't like your usual billionaire. The eccentric figure often does things differently and it often shows. \r\nElon Musk may b… [+1944 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Www.hln.be"
+    },
+    "author": "TVdB",
+    "title": "Splinternieuwe Tesla Model S Plaid vat plots vuur: chauffeur weet op nippertje aan vlammen te ontsnappen",
+    "description": "In Haverford, in de Amerikaanse staat Pennsylvania, heeft een splinternieuwe Tesla Model S Plaid vuur gevat. De chauffeur van de wagen raakte even ingesloten door de vlammen, maar wist uiteindelijk zonder enige verwonding te ontkomen. Volgens een van de advoc…",
+    "url": "https://www.hln.be/buitenland/splinternieuwe-tesla-model-s-plaid-vat-plots-vuur-chauffeur-weet-op-nippertje-aan-vlammen-te-ontsnappen~a14bce0b/",
+    "urlToImage": "https://images0.persgroep.net/rcs/1uiEroc5XPM7LaRC7CBt_P6f188/diocontent/204683054/_fill/1200/630/?appId=21791a8992982cd8da851550a453bd7f&quality=0.7",
+    "publishedAt": "2021-07-03T13:19:16Z",
+    "content": "De lokale brandweer had meer dan twee uur nodig om de wagen volledig te blussen. Het grootste deel van de carrosserie en de binnenkant van de auto is dan ook volledig verbrand. We vragen aan Tesla om… [+1807 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Komputerswiat.pl"
+    },
+    "author": "Joachim Snoch",
+    "title": "Bird zaoferuje do wynajmu elektryczne wózki inwalidzkie",
+    "description": "Bird to firma znana najbardziej z udostępniania w miejskich terenach elektrycznych hulajnóg. Teraz jednak rozszerza swoje portfolio i będzie oferować również… wózki inwalidzkie.",
+    "url": "https://www.komputerswiat.pl/aktualnosci/inne/bird-zaoferuje-do-wynajmu-elektryczne-wozki-inwalidzkie/6d1yfws",
+    "urlToImage": "https://ocdn.eu/pulscms-transforms/1/b2nktkpTURBXy81YTZhOWJjZjQ5ZGMxYWUyOTMwZGI1ZWU2Yjg4YjQyNi5qcGeSlQMAEM0E4s0Cv5MFzQSwzQJ2",
+    "publishedAt": "2021-07-03T13:12:00Z",
+    "content": "Bird niedawno zacz udostpnia rowery w ramach poruszania si po miecie, a teraz wkracza mocniej w obszary tzw. dostpnoci, czyli pomocy dla osób z niepenosprawnociami. Firma wraz ze Scootaround wdraa pr… [+877 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Diariomotor.com"
+    },
+    "author": "José Luis Gómez",
+    "title": "Estos son los diez coches más vendidos en junio, un mes con doble punto de inflexión",
+    "description": "Los datos de matriculaciones del primer semestre del año no son para nada positivos, pero lo cierto es los registrados en junio invitan a la esperanza. Se han matriculado 96.785 unidades, es decir, un 23,5 % menos que en junio de 2019, lo cual es un gran desc…",
+    "url": "https://www.diariomotor.com/noticia/matriculaciones-junio-2021/",
+    "urlToImage": "https://www.diariomotor.com/imagenes/2021/07/matriculaciones-junio-2021-fiat-500-01.jpg",
+    "publishedAt": "2021-07-03T12:58:55Z",
+    "content": "Los datos de matriculaciones del primer semestre del año no son para nada positivos, pero lo cierto es los registrados en junio invitan a la esperanza. Se han matriculado 96.785 unidades, es decir, u… [+3606 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "La Tribune.fr"
+    },
+    "author": null,
+    "title": "Robinhood, entre la Bourse et la vie",
+    "description": "Adulée et détestée, la plateforme de courtage en ligne Robinhood vient déposer son prospectus en vue d’une introduction en Bourse six ans seulement après son lancement. Une opération qui pourrait valoriser le courtier autour de 40 milliards de dollars. Son mo…",
+    "url": "https://www.latribune.fr/entreprises-finance/banques-finance/industrie-financiere/robinhood-entre-la-bourse-et-la-vie-888283.html",
+    "urlToImage": "https://static.latribune.fr/full_width/1725216/wall-street-termine-en-hausse.jpg",
+    "publishedAt": "2021-07-03T12:51:00Z",
+    "content": "Robinhood voit grand. Rendue célèbre par son rôle clé dans la folle spéculation boursière sur des valeurs décotées à Wall Street en début d'année, la plateforme de courtage en ligne préférée des Amér… [+8187 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Wykop.pl"
+    },
+    "author": "TechnoBoss",
+    "title": "2 sekundy do “setki” i 3 godziny do ugaszenia pożaru - nowa Tesla w skrócie",
+    "description": "Nowy samochód ze stajni Tesli zaczął docierać do klientów. Nie obyło się bez problemów już na samym początku. Jeden z Modeli S Plaid samoczynnie się zapalił - z kierowcą w środku.",
+    "url": "https://www.wykop.pl/link/6174329/2-sekundy-do-setki-i-3-godziny-do-ugaszenia-pozaru-nowa-tesla-w-skrocie/",
+    "urlToImage": "https://www.wykop.pl/cdn/c3397993/link_1625316094QQZj46u04NTG6nhLBcjNgq,w1200h627f.jpg",
+    "publishedAt": "2021-07-03T12:42:29Z",
+    "content": "Drogi Wykopowiczu\r\nw zaczonym linku do Polityki Prywatnoci przypominamy podstawowe informacje z zakresu przetwarzania danych osobowych dostarczanych przez Ciebie podczas korzystania z naszego serwisu… [+108 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Hvg.hu"
+    },
+    "author": "hvg@hvg.hu",
+    "title": "Teslát lopott a szerető, de egy fegyvert is vitt vele a csomagtartóban",
+    "description": "A kocsi gazdáját bezárta egy szobába, majd Pestre hajtott, de hamar lekapcsolták. A csomagtartóban talált tárgyat és lőszereket fegyverszakértő vizsgálja.",
+    "url": "https://hvg.hu/itthon/20210703_autolopas_tesla_fegyver_erd_budapest_szereto",
+    "urlToImage": "https://images.hvg.hu/image.jpg?id=2c9c6d23-0daa-4511-8cfe-579a64313da9&view=7fcefbf8-ac48-4ee6-aef5-32203afa118c",
+    "publishedAt": "2021-07-03T12:42:00Z",
+    "content": "A kocsi gazdáját bezárta egy szobába, majd Pestre hajtott, de hamar lekapcsolták. A csomagtartóban talált tárgyat és lszereket fegyverszakért vizsgálja.Úgy lopta el szeretje elektromos autóját egy 30… [+846 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "CNET"
+    },
+    "author": "Sean Szymkowski",
+    "title": "Biden and the bipartisan infrastructure plan: What's it mean for EV subsidies and credits?",
+    "description": "The original plan Biden proposed called for direct subsidies. That's not the case any longer.",
+    "url": "https://www.cnet.com/roadshow/news/ev-tax-credit-subsidy-ev-biden-infrastructure-plan/",
+    "urlToImage": "https://www.cnet.com/a/img/TlF0fQNVXgh72xXLC3J6vVVQLZU=/1200x630/2021/06/21/f3ae24c5-060a-4db3-8e28-2fe4e64d403f/ogi-cash.jpg",
+    "publishedAt": "2021-07-03T12:30:04Z",
+    "content": "Will we still see EV subsidies? Perhaps.\r\nSarah Tew/CNET\r\nThose hoping for direct subsidies to offset the price of a new electric vehicle won't see it in President Biden's newly negotiated, bipartisa… [+6242 chars]"
+    },
+    {
+    "source": {
+    "id": "the-verge",
+    "name": "The Verge"
+    },
+    "author": "Andrew J. Hawkins",
+    "title": "Elon Musk’s Boring Company pitches underground transit loop to Fort Lauderdale",
+    "description": "Fort Lauderdale says it has received a proposal for an underground tunnel from Elon Musk’s Boring Company. It is the latest city to become transfixed by Musk’s “Tesla in a tunnel” concept.",
+    "url": "https://www.theverge.com/2021/7/3/22559166/elon-musk-boring-company-fort-lauderdale-loop",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/5GL1SI3Qtxk3_fAgyaAEga07d4U=/0x382:5410x3214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22693558/1311709601.jpg",
+    "publishedAt": "2021-07-03T12:30:00Z",
+    "content": "The latest city to become transfixed by Musks Tesla in a tunnel concept\r\nPhoto by Ethan Miller/Getty Images\r\nThe Boring Company, Elon Musks tunneling venture, pitched an underground transit system to… [+2558 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "N-tv.de"
+    },
+    "author": "n-tv NACHRICHTEN",
+    "title": "Mission Satelliteninternet: Musk-Kritiker positionieren sich gegen Starlink",
+    "description": "42.000 Satelliten will SpaceX- und Starlink-Chef Elon Musk im Orbit platzieren. Das Ziel: schnelles Internet an jedem Ort der Welt, ohne den gigantischen Aufwand, Glasfaserkabel verlegen zu müssen. Was eine tolle Idee scheint, hat jedoch Haken, die Musk bisla…",
+    "url": "https://www.n-tv.de/wirtschaft/Musk-auf-Starlink-Mission-Wird-Satelliteninternet-zur-Gefahr-article22658882.html",
+    "urlToImage": "https://bilder3.n-tv.de/img/incoming/crop22653238/3651327721-cImg_16_9-w1200/962aaaecb5c4e61eff570875d619f0cb.jpg",
+    "publishedAt": "2021-07-03T12:25:19Z",
+    "content": "42.000 Satelliten will SpaceX- und Starlink-Chef Elon Musk im Orbit platzieren. Das Ziel: schnelles Internet an jedem Ort der Welt, ohne den gigantischen Aufwand, Glasfaserkabel verlegen zu müssen. W… [+8391 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Forbes"
+    },
+    "author": "David Blackmon, Senior Contributor, \n David Blackmon, Senior Contributor\n https://www.forbes.com/sites/davidblackmon/",
+    "title": "ExxonMobil Employs Lobbyists? Who Knew?",
+    "description": "Ok, so, the big ‘scandal’ drummed up by Greenpeace this past week was the ‘news’ that ExxonMobil employs a lobbyist who boasts of doing his aggressively.",
+    "url": "https://www.forbes.com/sites/davidblackmon/2021/07/03/exxonmobil-employs-lobbyists-who-knew/",
+    "urlToImage": "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F60df41ce6855f2159caa4e5b%2F0x0.jpg%3FcropX1%3D0%26cropX2%3D2553%26cropY1%3D145%26cropY2%3D1581",
+    "publishedAt": "2021-07-03T12:15:10Z",
+    "content": "Streetsign for K Street, the Wall Street of political influence in the US capital. Sunrise in the ... [+] district of the political lobbyist.\r\ngetty\r\nThe big scandal drummed up by Greenpeace this pas… [+5806 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Ferra.ru"
+    },
+    "author": "Ксения Мурашева",
+    "title": "Армию России сравнили с «Запорожцем»",
+    "description": "Бывший американский военный порассуждал об армиях мира. Российская получила не самый лестный отзыв.",
+    "url": "https://www.ferra.ru/news/techlife/armiyu-rossii-sravnili-s-zaporozhcem-03-07-2021.htm",
+    "urlToImage": "https://www.ferra.ru/imgs/2021/07/03/12/4747682/e0eb99c12b37734a874dbcf6d8b9b7c9c5007da3.jpg",
+    "publishedAt": "2021-07-03T12:09:27Z",
+    "content": "- () «.». «», Tesla. \r\n , . , . - , . \r\n . , 2014"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Berliner Morgenpost"
+    },
+    "author": "Oliver Stöwing",
+    "title": "Weltraumtourismus: Warum Richard Branson der erste Milliardär im All sein will",
+    "description": "Am 20. Juli will Amazon-Chef Bezos mit seinem eigenen Raumschiff den Planeten verlassen. Ein anderer Milliardär will ihm zuvorkommen.",
+    "url": "https://www.morgenpost.de/vermischtes/article232696401/Warum-Richard-Branson-der-erste-Milliardaer-im-All-sein-will.html",
+    "urlToImage": "https://img.morgenpost.de/img/vermischtes/crop232696399/7662605996-w820-cv16_9-q85/cf449598-db40-11eb-89f2-eef5f2aaea16.jpg",
+    "publishedAt": "2021-07-03T12:07:46Z",
+    "content": "Spaceport America. Nur der Himmel ist die Grenze mit dieser Phrase motivieren sich oft Menschen, die sich für Siegertypen halten. Für Multimilliardäre wie Jeff Bezos (57) oder Elon Musk (50) jedoch g… [+3364 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "CleanTechnica"
+    },
+    "author": "Zachary Shahan",
+    "title": "Tesla Model 3, Model Y, Model S, & Model X Sales Estimates — 1st Half Of 2021",
+    "description": "I reported yesterday on Tesla’s record quarterly production and deliveries (aka sales). It’s another stunning feat. Tesla’s the first company in the world to sell more than 200,000 electric vehicles in a quarter. But one unfortunate thing about Tesla’s offici…",
+    "url": "https://cleantechnica.com/2021/07/03/tesla-model-3-model-y-model-s-model-x-sales-estimates-1st-half-of-2021/",
+    "urlToImage": "https://cleantechnica.com/files/2021/07/Q2-Tesla-vehicle-sales-quarterly-deliveries-CleanTechnica-logo.png",
+    "publishedAt": "2021-07-03T12:06:11Z",
+    "content": "I reported yesterday on Tesla’s record quarterly production and deliveries (aka sales). It’s another stunning feat. Tesla’s the first company in the world to sell more than 200,000 electric vehicles … [+2120 chars]"
+    }
+    ]
+    }
