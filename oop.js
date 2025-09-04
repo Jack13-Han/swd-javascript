c0cef59d4da0a29ee400ce4c89f252039ce994a1
Jack13-Han <hanwaihtun1e.com>
2025-09-04 15:40:22 +0900

    new js

--- oop.js
+++ oop.js
@@ -1,24 +1,50 @@
-class Person{
-    constructor(name,age,skinColor){
-        this.name=name;
-        this.age=age;
-        this.skinColor=skinColor;
-    }
-}
+// class Person{
+//     constructor(name,age,skinColor){
+//         this.name=name;
+//         this.age=age;
+//         this.skinColor=skinColor;
+//     }
+// }
 
+// class Student extends Person {
+//     constructor(name,age,MajorName) {
+//         super(name,age); Parent ထဲက ဟာကိုပြန်ခေါ်သုံးချင်ရင် super နဲ့ပြန်ခေါ်သုံးတယ်
 
-class Student extends Person {
-    constructor(name,age,MajorName) {
-        super(name,age);
-        this.classRoom=MajorName;
-    }
-}
+//         this.classRoom=MajorName;
+//     }
+// }
 
+// const p1 = new Person("Mg Aye",25,"Black");
 
-const p1 = new Person("Mg Aye",25,"Black");
+// console.log(p1)
 
-console.log(p1)
+// const s1=new Student("Mr.Jack",23,"White","Cu")
 
-const s1=new Student("Mr.Jack",23,"White","Cu")
+// console.log(s1)
 
-console.log(s1)
+const date = new Date();
+
+const fullMonthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+//To နဲ့က အတွဲလိုက် ယူတာ ဖြစ်ပါတယ်
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(date.toTimeString())
+
+console.log(fullMonthNames[date.getMonth()]);
+console.log(date.getFullYear());
+console.log(date.getYear());
